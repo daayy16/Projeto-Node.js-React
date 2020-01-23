ff3ed8b4d2c1fe8a204e3d99bf5c36d58b214fa3
daayy16 <daiane.candido29@yahoo.com.br>
2020-01-23 12:52:00 -0300

    Aplicação criada na semana omnstack da rocketseat

---
+++ Aplicacao-Backend/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http =  require('http');
+const routes = require('./routes.js');
+const { setupWebsocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect('mongodb://localhost:27017/week10', { useNewUrlParser: true,
+useUnifiedTopology: true});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3329);
